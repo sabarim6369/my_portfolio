b1c722ae7e44729eb62b01487f24dd00a7e2d9cc
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-01-17 07:08:18 +0530

    Update backend api

--- Documents/Projects/My_Portfolio/portfolio/src/components/Contact/Contact.jsx
+++ Documents/Projects/My_Portfolio/portfolio/src/components/Contact/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://my-portfolio-1-n68t.onrender.com/contact", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
