f07c80111af82f56f0e694e5b620979dbe805497
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-01-17 06:57:10 +0530

    Update Projects.jsx

--- Documents/Projects/My_Portfolio/portfolio/src/components/Projects/Projects.jsx
+++ Documents/Projects/My_Portfolio/portfolio/src/components/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Github, ExternalLink, Briefcase, Code } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';  
-import { Link } from 'react-router-dom';
+
 import { useInView } from 'react-intersection-observer';
 
 const Projects = () => {
