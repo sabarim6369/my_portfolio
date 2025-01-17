54103256f03b9c573ccdf3f557fde04dbc2c1134
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-01-17 06:50:53 +0530

    Update App.js

--- Documents/Projects/My_Portfolio/portfolio/src/App.js
+++ Documents/Projects/My_Portfolio/portfolio/src/App.js
@@ -5,23 +5,24 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+
 const App = () => {
   return (
     <div className="relative min-h-screen w-full">
       {/* Fixed video container */}
       <div className="fixed top-0 left-0 w-full h-full">
-        // <video
-        //   className="absolute top-0 left-0 w-full h-full object-cover"
-        //   autoPlay
-        //   loop
-        //   muted
-        //   playsInline
-        // >
-        //   <source src={video1} type="video/mp4" />
-        //   <source src={video1} type="video/webm" />
-        //   <source src={video1} type="video/ogg" />
-        //   Your browser does not support the video tag.
-        // </video>
+        {/* <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src={video1} type="video/mp4" />
+          <source src={video1} type="video/webm" />
+          <source src={video1} type="video/ogg" />
+          Your browser does not support the video tag.
+        </video> */}
       </div>
 
       {/* Scrollable content */}
@@ -30,7 +31,7 @@ const App = () => {
         <Home />
         <About />
         <Projects />
-        <Contact/>
+        <Contact />
       </div>
     </div>
   );
